35d626b06a093b1447ef92eb3b572317e4774d50
Alethea Cary <aletheacary@gmail.com>
2019-06-26 00:59:19 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,11 +1,11 @@
 # Create a class and inherit from ActiveRecord::Migration
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :string
+      t.string :size
       t.text :image_url
       t.datetime :created_at
       t.datetime :updated_at
---
+++ db/migrate/004_correct_spelling_and_type_errors_for_costume_stores.rb
@@ -0,0 +1,11 @@
+# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    change_column :costume_stores, :opening_time, :datetime
+    change_table :costume_stores do |t|
+      t.rename :constume_inventory, :costume_inventory
+    end
+  end
+end
+    
+  
